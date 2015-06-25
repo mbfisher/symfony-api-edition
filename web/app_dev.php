3131f4b23e4058cd0ef635ddfd5ad47ea049429c
Mike Fisher <mike@mbfisher.com>
2015-06-25 09:54:22 +0100

    Add production FrontController

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,7 +3,6 @@
 use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__.'/../vendor/autoload.php';
-
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
